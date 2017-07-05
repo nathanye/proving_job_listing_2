448ad55a40ad2e5cba16023c8a3c3e26dcba39e6
nathanye <khalil54781@gmail.com>
2017-07-05 16:19:48 +0800

    jobs CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
     @jobs = Job.all
@@ -47,4 +47,5 @@ class JobsController < ApplicationController
   def job_params
     params.require(:job).permit(:title, :description)
   end
+
 end
